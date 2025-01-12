e1f0131cf5193c69ed4e809f06e3a9c5472c2f4d
Ingryd <ingrydcduarte@gmail.com>
2025-01-12 20:06:38 -0300

    add: minimal renderer.js and setup electron

---
+++ js/classes.js
@@ -0,0 +1,112 @@
+class Player {
+    constructor(x, y) {
+      this.pos = createVector(x, y);
+      this.vel = 3;
+      this.sprite = spr1;
+      this.bullets = [];
+      this.tam = 32;
+      this.mouseHeld = false;
+    }
+  
+    update() {
+      this.show();
+      this.move();
+      this.checkParede();
+      this.verificaAtaque();
+      
+    }
+  
+    show() {
+      image(this.sprite, this.pos.array()[0] - this.tam, this.pos.array()[1]);
+    }
+  
+    move() {
+      if (keyIsDown(RIGHT_ARROW)) this.pos.x += this.vel;
+      if (keyIsDown(LEFT_ARROW)) this.pos.x -= this.vel;
+      // if (keyIsDown(DOWN_ARROW)) this.pos.y += this.vel;
+      // if (keyIsDown(UP_ARROW)) this.pos.y -= this.vel;
+    }
+  
+    verificaAtaque() {
+      if (mouseIsPressed && mouseButton == LEFT && !this.mouseHeld) {
+        this.bullets.push(new Bullet(this));
+        this.mouseHeld = true;
+      } else if (!mouseIsPressed) {
+        this.mouseHeld = false;
+      }
+  
+      //atualiza cada valor dentro do array bullets
+      for (let tiro of this.bullets) {
+        tiro.update();
+      }
+    }
+  
+    //TODO: Colisão com parede
+    checkParede() {
+      if (this.pos.x + this.tam >= width) {
+        this.pos.x = width - this.tam / 2;
+      } else if (this.pos.x - this.tam <= 0) {
+        this.pos.x = this.tam;
+      }
+    }
+  }
+  
+  class Enemy {
+    constructor(x, y) {
+      this.pos = createVector(x, y);
+      this.vel = 1.5;
+      this.sprite = spr2;
+      this.tam = 32;
+    }
+  
+    updateEnemy() {
+      this.show();
+      this.move();
+    }
+  
+    show() {
+      image(this.sprite, this.pos.array()[0] - this.tam, this.pos.array()[1]);
+    }
+  
+    move() {
+      this.pos.y += this.vel;
+  
+      if (this.pos.y > height) {
+        this.pos.y = 150;
+      }
+    }
+  
+    //Colisão tiro - inimigo
+    checkCollision(bullet) {
+      return (
+        bullet.x > this.pos.x - this.tam &&
+        bullet.x < this.pos.x + this.tam &&
+        bullet.y > this.pos.y - this.tam &&
+        bullet.y < this.pos.y + this.tam
+      );
+    }
+  }
+  
+  class Bullet {
+    constructor(objJogador) {
+      this.x = objJogador.pos.x;
+      this.y = objJogador.pos.y;
+      this.vel = 1.5;
+    }
+  
+    update() {
+      this.show();
+      this.y -= this.vel;
+    }
+  
+    show() {
+      rect(this.x, this.y, 5, 10);
+    }
+    
+    foraDeZona(){
+      
+    }
+  
+    //TODO: Colisão tiro - inimigo
+  }
+  
---
+++ js/renderer.js
@@ -0,0 +1,142 @@
+var tela = 0;
+let enemy = [],
+  player,
+  bg,
+  spr1,
+  spr2,
+  gif,
+  owner;
+let score = 0;
+
+function preload() {
+  spr1 = loadImage("./assets/spr1.png");
+  spr2 = loadImage("./assets/spr2.png");
+  bg = loadImage("./assets/bg.png");
+  owner = loadImage("./assets/me-pixel.png");
+}
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  noCursor();
+  player = new Player(width / 2, height - 80);
+
+  for (let i = 0; i < 3; i++) {
+    enemy[i] = new Enemy(int(random(0, width)), 180);
+  }
+}
+
+function draw() {
+  background(0);
+
+  switch (tela) {
+    case 0:
+      homeGame();
+      break;
+    case 1:
+      mainGame();
+      break;
+    case 2:
+      loseScreen();
+      break;
+    case 3:
+      apOwner();
+      break;
+  }
+
+  mira();
+  noStroke();
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+  player.x = width / 2; // Reposiciona o jogador ao centro
+}
+
+function mira() {
+  stroke("#AE35D1");
+  strokeWeight(2);
+  line(mouseX - 10, mouseY, mouseX + 10, mouseY); 
+  line(mouseX, mouseY - 10, mouseX, mouseY + 10);
+  noFill();
+}
+
+function homeGame() {
+  fill("#fafafa");
+  textAlign(CENTER);
+  textStyle(BOLD);
+  textSize(40);
+  text("BEAM RIDER", width / 2, height / 2 - 50);
+
+  // Botão "JOGAR"
+  fill("#fafafa");
+  rect(width / 4, height / 2 + 15, width / 2, 50);
+  fill("#161616");
+  textSize(20);
+  text("JOGAR", width / 2, height / 2 + 45);
+
+  // Botão "AP1"
+  fill("#fafafa");
+  rect(width / 4, height / 2 + 75, width / 2, 50);
+  fill("#161616");
+  text("AP1", width / 2, height / 2 + 105);
+
+  // Interação dos botões
+  if (mouseIsPressed) {
+    if (mouseX >= width / 4 && mouseX <= (width / 4) + (width / 2)) {
+      if (mouseY >= height / 2 + 15 && mouseY <= height / 2 + 65) {
+        tela = 1; // Vai para o jogo
+      } else if (mouseY >= height / 2 + 75 && mouseY <= height / 2 + 125) {
+        tela = 3; // Vai para a tela de créditos
+      }
+    }
+  }
+}
+
+function mainGame() {
+  image(bg, 0, 0, width, height * 0.375);
+  fill("#DB8E23");
+  textAlign(CENTER);
+  text("SECTION " + score, width / 2, 50);
+
+  player.update();
+  for (let i = enemy.length - 1; i >= 0; i--) {
+    enemy[i].updateEnemy();
+
+    for (let j = player.bullets.length - 1; j >= 0; j--) {
+      if (enemy[i].checkCollision(player.bullets[j])) {
+        enemy.splice(i, 1); // Remove o inimigo
+        score += 5;
+        player.bullets.splice(j, 1); // Remove o tiro
+        break;
+      }
+    }
+  }
+}
+
+function apOwner() {
+  const pX = 30,
+    pY = 30;
+  image(owner, width / 2 - 75, height / 2 - 100, 150, 150);
+
+  fill("#fafafa");
+  textAlign(CENTER);
+  textSize(25);
+  text("Ingryd Duarte. M: 570661", width / 2, height / 2 + 100);
+
+  rect(pX, pY, 80, 40);
+  fill("#161616");
+  textSize(15);
+  text("Voltar", pX + 40, pY + 25);
+
+  // Botão de volta
+  if (mouseIsPressed) {
+    if (
+      mouseX >= pX &&
+      mouseX <= pX + 80 &&
+      mouseY >= pY &&
+      mouseY <= pY + 40
+    ) {
+      tela = 0; // Voltar à tela inicial
+    }
+  }
+}
